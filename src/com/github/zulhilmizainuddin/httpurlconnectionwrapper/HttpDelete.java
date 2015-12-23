6fdb7db8579c94b11a8b3bd6ea921d8ded2e1301
ZulhilmiZainuddin <zulhilmi.zainuddin@outlook.com>
2015-12-23 13:56:37 +0800

    HTTP DELETE implementation.

---
+++ src/com/github/zulhilmizainuddin/httpurlconnectionwrapper/HttpDelete.java
@@ -0,0 +1,75 @@
+package com.github.zulhilmizainuddin.httpurlconnectionwrapper;
+
+
+import java.io.IOException;
+import java.net.HttpCookie;
+import java.util.List;
+import java.util.Map;
+
+public final class HttpDelete extends Http implements HttpBuilder {
+
+    public HttpDelete(String url) throws IOException {
+        super(url);
+        connection.setRequestMethod(HttpVerb.DELETE);
+    }
+
+    @Override
+    public Http setRequestHeaders(Map<String, String> requestHeaders) {
+        parameter.setRequestHeaders(requestHeaders);
+        return this;
+    }
+
+    @Override
+    public Http setCookies(List<HttpCookie> cookies) {
+        parameter.setCookies(cookies);
+        return this;
+    }
+
+    @Override
+    public Http setConnectionTimeout(int connectionTimeout) {
+        parameter.setConnectionTimeout(connectionTimeout);
+        return this;
+    }
+
+    @Override
+    public Http setReadTimeout(int readTimeout) {
+        parameter.setReadTimeout(readTimeout);
+        return this;
+    }
+
+    @Override
+    public Http setRequestBody(String requestBody) {
+        throw new UnsupportedOperationException("Not supported for HTTP DELETE");
+    }
+
+    @Override
+    public Http setFollowRedirects(boolean followRedirects) {
+        parameter.setFollowRedirects(followRedirects);
+        return this;
+    }
+
+    @Override
+    public int execute() throws IOException {
+        int responseCode = -1;
+
+        try {
+            setRequestHeaders();
+            setCookies();
+            setConnectionTimeout();
+            setReadTimeout();
+            setFollowRedirects();
+
+            responseCode = connection.getResponseCode();
+
+            retrieveResponseHeaders();
+            retrieveResponseCookies();
+            retrieveRedirectUrl();
+            retrieveResponseBody();
+        }
+        finally {
+            connection.disconnect();
+        }
+
+        return responseCode;
+    }
+}
--- src/com/github/zulhilmizainuddin/httpurlconnectionwrapper/HttpVerb.java
+++ src/com/github/zulhilmizainuddin/httpurlconnectionwrapper/HttpVerb.java
@@ -5,4 +5,5 @@ public final class HttpVerb {
     public static final String GET = "GET";
     public static final String POST = "POST";
     public static final String PUT = "PUT";
+    public static final String DELETE = "DELETE";
 }
